bcd85d507713605b89e21e6b49e9e2a334d633eb
Mohan Vaddi <myselfgeek01@gmail.com>
2022-03-28 04:24:00 +0530

    changes

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -3,6 +3,19 @@ import TextureKeys from './../consts/Texturekeys';
 import SceneKeys from '../consts/SceneKeys';
 import AnimationKeys from '../consts/AnimationKeys';
 
+import backgroundImg from '../../house/bg_repeat_340x640.png';
+import mouseHoleImg from '../../house/object_mousehole.png';
+import window1Img from '../../house/object_window1.png';
+import window2Img from '../../house/object_window2.png';
+import bookCaseImg1 from '../../house/object_bookcase1.png';
+import bookCaseImg2 from '../../house/object_bookcase2.png';
+import laserEndImg from '../../house/object_laser_end.png';
+import laserImg from '../../house/object_laser.png';
+import coinImg from '../../house/object_coin.png';
+import rocketMouseImg from '../../characters/rocket-mouse.png';
+
+import rocketMouseJson from '../../characters/rocket-mouse.json';
+
 // This is also true of loaded assets like images and atlases.
 // Once they are loaded, they can be accessed by any Scene.
 
@@ -13,46 +26,31 @@ export default class Preloader extends Phaser.Scene {
 
     preload() {
         // * load background
-        this.load.image(TextureKeys.Background, '/house/bg_repeat_340x640.png');
+        this.load.image(TextureKeys.Background, backgroundImg);
 
         // * load mouseHole
-        this.load.image(
-            TextureKeys.MouseHole,
-            '../../house/object_mousehole.png'
-        );
+        this.load.image(TextureKeys.MouseHole, mouseHoleImg);
 
         // * load windows
-        this.load.image(TextureKeys.Window1, '../../house/object_window1.png');
-        this.load.image(TextureKeys.Window2, '../../house/object_window2.png');
+        this.load.image(TextureKeys.Window1, window1Img);
+        this.load.image(TextureKeys.Window2, window2Img);
 
         // * load bookcases
-        this.load.image(
-            TextureKeys.Bookcase1,
-            '../../house/object_bookcase1.png'
-        );
-        this.load.image(
-            TextureKeys.Bookcase2,
-            '../../house/object_bookcase2.png'
-        );
+        this.load.image(TextureKeys.Bookcase1, bookCaseImg1);
+        this.load.image(TextureKeys.Bookcase2, bookCaseImg2);
 
         // * load lasers
-        this.load.image(
-            TextureKeys.LaserEnd,
-            '../../house/object_laser_end.png'
-        );
-        this.load.image(
-            TextureKeys.LaserMiddle,
-            '../../house/object_laser.png'
-        );
+        this.load.image(TextureKeys.LaserEnd, laserEndImg);
+        this.load.image(TextureKeys.LaserMiddle, laserImg);
 
         // * load coin image
-        this.load.image(TextureKeys.Coin, '../../house/object_coin.png');
+        this.load.image(TextureKeys.Coin, coinImg);
 
         //* sprite sheets are loaded as atlas
         this.load.atlas(
             TextureKeys.RocketMouse,
-            '../../characters/rocket-mouse.png',
-            '../../characters/rocket-mouse.json'
+            rocketMouseImg,
+            rocketMouseJson
         );
     }
     create() {
